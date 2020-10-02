dc43e71bbb15e71c319b1148fb6f7743b589ebbb
Yvo Brevoort <yvo@muze.nl>
2020-10-02 20:30:09 +0200

    added token response with id token

--- src/Controller/TokenController.php
+++ src/Controller/TokenController.php
@@ -10,10 +10,11 @@ class TokenController extends ServerController
 {    
     final public function __invoke(ServerRequestInterface $request, array $args): ResponseInterface
     {	
-		$code = $_POST['code'];
-		$clientId = $_POST['client_id'];
+		$code = $request->$request->getParsedBody()['code'];
+		$clientId = $request->getParsedBody()['client_id'];
+
+/*
 		$DPoP = $_SERVER['HTTP_DPOP'];
-		
 		$parser = new \Lcobucci\JWT\Parser();
 		try {
 			$token = $parser->parse($DPoP);
@@ -21,7 +22,7 @@ class TokenController extends ServerController
 		} catch(\Exception $e) {
 			return $this->getResponse()->withStatus(409, "Invalid token");
 		}
-		
+*/
 		$registration = $this->config->getClientRegistration($clientId);
 		$approval = $this->checkApproval($clientId);
 		
@@ -29,11 +30,9 @@ class TokenController extends ServerController
 			$response = new \Laminas\Diactoros\Response();
 			$server	= new \Pdsinterop\Solid\Auth\Server($this->authServerFactory, $this->authServerConfig, $response);
 			$response = $server->respondToAccessTokenRequest($request);
+			$response = $this->tokenGenerator->addIdTokenToResponse($response, $clientId, $this->getProfilePage(), $_SESSION['nonce'], $this->config->getPrivateKey());
 
-//			$response = $this->tokenGenerator->addIdTokenToResponse($response, $clientId, $this->getProfilePage(), $_SESSION['nonce'], $this->config->getPrivateKey());
 			return $response;
-//			$idToken = $this->tokenGenerator->generateIdToken($code, $clientId, $this->getProfilePage(), $_SESSION['nonce'], $this->config->getPrivateKey());
-	//		return new JsonResponse(array("token_type" => "DPoP", "id_token" => $idToken));
 		}
 		return new JsonResponse(array());
     }
