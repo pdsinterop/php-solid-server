4672c4bb8ac7dc2d0c7904d01f30263e955acab6
Michiel de Jong <michiel@unhosted.org>
2020-10-19 13:48:46 +0200

    Build and run solid-crud tests

--- run-solid-test-suite.sh
+++ run-solid-test-suite.sh
@@ -9,6 +9,7 @@ docker build -t server .
 docker run -d --name server --network=testnet server
 
 docker build -t webid-provider https://github.com/pdsinterop/test-suites.git#master:/testers/webid-provider
+docker build -t solid-crud https://github.com/pdsinterop/test-suites.git#master:/testers/solid-crud
 docker build -t cookie         https://github.com/pdsinterop/test-suites.git#master:servers/php-solid-server/cookie
 wget -O /tmp/env-vars-for-test-image.list https://raw.githubusercontent.com/solid/test-suite/master/servers/php-solid-server/env.list
 until docker run --rm --network=testnet webid-provider curl -kI https://server 2> /dev/null > /dev/null
@@ -25,6 +26,7 @@ echo Getting cookie...
 export COOKIE="`docker run --cap-add=SYS_ADMIN --network=testnet --env-file /tmp/env-vars-for-test-image.list cookie`"
 echo "Running webid-provider tests with cookie $COOKIE"
 docker run --rm --network=testnet --env COOKIE="$COOKIE" --env-file /tmp/env-vars-for-test-image.list webid-provider
+docker run --rm --network=testnet --env COOKIE="$COOKIE" --env-file /tmp/env-vars-for-test-image.list solid-crud
 # rm /tmp/env-vars-for-test-image.list
 # docker stop server
 # docker rm server
