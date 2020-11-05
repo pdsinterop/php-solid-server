6544379e6c8d31c16f5ced729f935bcd956ac6d3
Michiel de Jong <michiel@unhosted.org>
2020-11-05 22:55:33 +0100

    use solid-crud Dockerfile from solid/test-suite

--- run-solid-test-suite.sh
+++ run-solid-test-suite.sh
@@ -8,8 +8,8 @@ docker network create testnet
 docker build -t standalone-solid-server .
 
 docker build -t webid-provider https://github.com/pdsinterop/test-suites.git#master:/testers/webid-provider
-#docker build -t solid-crud https://github.com/pdsinterop/test-suites.git#master:/testers/solid-crud
-docker build --no-cache -t solid-crud https://github.com/pdsinterop/test-suites.git#master:/testers/solid-crud
+#docker build -t solid-crud https://github.com/solid/test-suite.git#master:/testers/solid-crud
+docker build --no-cache -t solid-crud https://github.com/solid/test-suite.git#master:/testers/solid-crud
 docker build -t cookie         https://github.com/pdsinterop/test-suites.git#master:servers/php-solid-server/cookie
 docker build -t pubsub-server  https://github.com/pdsinterop/php-solid-pubsub-server.git#master
 
