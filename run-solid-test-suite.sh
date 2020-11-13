107e9994a5efd28744d618ce5be731d547f31c72
Michiel de Jong <michiel@unhosted.org>
2020-11-13 15:20:11 +0100

    Revert last commit

--- run-solid-test-suite.sh
+++ run-solid-test-suite.sh
@@ -6,15 +6,19 @@ function setup {
   docker network create testnet
 
   # Build and start Nextcloud server with code from current repo contents:
-  docker build -t standalone-solid-server .
+  docker build --no-cache -t standalone-solid-server .
+
+  docker build -t webid-provider https://github.com/pdsinterop/test-suites.git#master:/testers/webid-provider
+  #docker build -t solid-crud https://github.com/solid/test-suite.git#master:/testers/solid-crud
+  docker build --no-cache -t solid-crud https://github.com/solid/test-suite.git#master:/testers/solid-crud
+  #docker build -t cookie         https://github.com/solid/test-suite.git#master:helpers/cookie
   docker build -t cookie         https://github.com/pdsinterop/test-suites.git#master:servers/php-solid-server/cookie
   docker build -t pubsub-server  https://github.com/pdsinterop/php-solid-pubsub-server.git#master
-  docker pull solidtestsuite/webid-provider-tests
-  docker pull solidtestsuite/solid-crud-tests
 }
 
 function runPss {
   docker run -d --name server --network=testnet --env-file ./env-vars-for-test-image.list standalone-solid-server
+
   docker run -d --name pubsub --network=testnet pubsub-server
 
   until docker run --rm --network=testnet webid-provider curl -kI https://server 2> /dev/null > /dev/null
@@ -33,8 +37,8 @@ function runPss {
 
 function runTests {
   echo "Running webid-provider tests with cookie $COOKIE"
-  docker run --rm --network=testnet --env COOKIE="$COOKIE" --env-file ./env-vars-for-test-image.list solidtestsuite/webid-provider-tests
-  docker run --rm --network=testnet --env-file ./env-vars-for-test-image.list solidtestsuite/solid-crud-tests
+  docker run --rm --network=testnet --env COOKIE="$COOKIE" --env-file ./env-vars-for-test-image.list webid-provider
+  docker run --rm --network=testnet --env-file ./env-vars-for-test-image.list solid-crud
 }
 
 function teardown {
