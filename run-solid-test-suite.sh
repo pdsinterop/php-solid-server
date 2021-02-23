5ae5d4f421cedd9f8acfdc30849a481cc0218e1d
Michiel de Jong <michiel@unhosted.org>
2021-02-23 11:33:40 +0100

    docker tag same as docker pull

--- run-solid-test-suite.sh
+++ run-solid-test-suite.sh
@@ -12,11 +12,11 @@ function setup {
   docker build -t pubsub-server  https://github.com/pdsinterop/php-solid-pubsub-server.git#master
 
   docker pull solidtestsuite/webid-provider-tests:v2.0.3
-  docker tag solidtestsuite/webid-provider-tests:latest webid-provider-tests
+  docker tag solidtestsuite/webid-provider-tests:v2.0.3 webid-provider-tests
   docker pull solidtestsuite/solid-crud-tests:v3.1.0
-  docker tag solidtestsuite/solid-crud-tests:latest solid-crud-tests
+  docker tag solidtestsuite/solid-crud-tests:v3.1.0 solid-crud-tests
   docker pull solidtestsuite/web-access-control-tests:v4.0.0
-  docker tag solidtestsuite/web-access-control-tests:latest web-access-control-tests
+  docker tag solidtestsuite/web-access-control-tests:v4.0.0 web-access-control-tests
 }
 
 function runPss {
