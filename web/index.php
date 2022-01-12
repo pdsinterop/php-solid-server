7b95c681c034bc11fb833cc840fd7ba69d122e8e
Ben Peachey <ben@muze.nl>
2022-01-12 10:49:09 +0100

    Change Storage directory to be set from environment.

--- web/index.php
+++ web/index.php
@@ -57,9 +57,9 @@ $container->add(ServerRequestInterface::class, Request::class);
 $container->add(ResponseInterface::class, Response::class);
 
 $container->share(FilesystemInterface::class, function () use ($request) {
-    // @FIXME: Filesystem root and the $adapter should be configurable.
+    // @FIXME: Filesystem $adapter should be configurable.
     //         Implement this with `$filesystem = \MJRider\FlysystemFactory\create(getenv('STORAGE_ENDPOINT'));`
-    $filesystemRoot = __DIR__ . '/../tests/fixtures';
+    $filesystemRoot = getenv('STORAGE_ENDPOINT') ?: __DIR__ . '/../tests/fixtures';
 
     $adapter = new \League\Flysystem\Adapter\Local($filesystemRoot);
 
